be0e74529d829fbe53bc0db0d285a53d99cd8812
Weaver23PH <37690862+Weaver23PH@users.noreply.github.com>
2018-10-24 12:49:56 +0200

    added javadoc

--- WashingMachine/src/washingMachine/WashingMachine.java
+++ WashingMachine/src/washingMachine/WashingMachine.java
@@ -1,5 +1,11 @@
 package washingMachine;
 
+/**
+ * @author MCARLO
+ */
+/**
+ * @author MCARLO
+ */
 public abstract class WashingMachine {
 	protected int	programme;
 	protected float	temperature;
@@ -20,6 +26,10 @@ public abstract class WashingMachine {
 		return programme;
 	}
 	
+	/**
+	 * @param programme
+	 * @return This setter sets programme and only allows values within the accepted limits
+	 */
 	public int setProgramme(int programme) {
 		if (programme <= 20) {
 			this.programme = programme;
@@ -33,13 +43,19 @@ public abstract class WashingMachine {
 		return temperature;
 	}
 	
+	/**
+	 * @param temperature
+	 * @return This setter sets temperature and rounds its value according to input
+	 */
 	public float setTemperature(float temperature) {
-		if (temperature - Math.floor(temperature) < 0.4) {
-			this.temperature = (float) Math.floor(temperature);
-		} else if (temperature - Math.floor(temperature) > 0.7) {
-			this.temperature = (float) Math.ceil(temperature);
-		} else {
-			this.temperature = (float) (Math.floor(temperature) + 0.5);
+		if (temperature <= 90.0f) {
+			if (temperature - Math.floor(temperature) < 0.4) {
+				this.temperature = (float) Math.floor(temperature);
+			} else if (temperature - Math.floor(temperature) > 0.7) {
+				this.temperature = (float) Math.ceil(temperature);
+			} else {
+				this.temperature = (float) (Math.floor(temperature) + 0.5);
+			}
 		}
 		return this.temperature;
 	}
@@ -48,6 +64,10 @@ public abstract class WashingMachine {
 		return rotationSpeed;
 	}
 	
+	/**
+	 * @param temperature
+	 * @return This setter sets rotation speed and rounds its value according to input
+	 */
 	public int setRotationSpeed(int rotationSpeed) {
 		if (rotationSpeed <= 1000) {
 			this.rotationSpeed = (int) ((Math.round(rotationSpeed / 100.0)) * 100);
@@ -57,6 +77,9 @@ public abstract class WashingMachine {
 		return this.rotationSpeed;
 	}
 	
+	/**
+	 * @return This method increases programme by 1
+	 */
 	public int nextProgramme() {
 		if (this.programme < 20) {
 			this.programme++;
@@ -64,6 +87,9 @@ public abstract class WashingMachine {
 		return this.programme;
 	}
 	
+	/**
+	 * @return This method decreases programme by 1
+	 */
 	public int previousProgramme() {
 		if (this.programme > 0) {
 			this.programme--;
@@ -71,6 +97,10 @@ public abstract class WashingMachine {
 		return this.programme;
 	}
 	
+	/**
+	 * @return This method increases temperature
+	 * @throws WashingMachineException
+	 */
 	public float tempUp() throws WashingMachineException {
 		if (this.temperature < 90) {
 			this.temperature += tempStep;
@@ -81,6 +111,10 @@ public abstract class WashingMachine {
 		}
 	}
 	
+	/**
+	 * @return This method decreases temperature
+	 * @throws WashingMachineException
+	 */
 	public float tempDown() throws WashingMachineException {
 		if (this.temperature > 0) {
 			this.temperature -= tempStep;
@@ -91,6 +125,9 @@ public abstract class WashingMachine {
 		}
 	}
 	
+	/**
+	 * @return This method increases rotation speed by 100
+	 */
 	public int rotationSpeeedUp() {
 		if (this.rotationSpeed < 1000) {
 			this.rotationSpeed += 100;
@@ -100,6 +137,9 @@ public abstract class WashingMachine {
 		return this.rotationSpeed;
 	}
 	
+	/**
+	 * @return This method decreases rotation speed by 100
+	 */
 	public int rotationSpeedDown() {
 		if (this.rotationSpeed > 0) {
 			this.rotationSpeed -= 100;
@@ -109,6 +149,9 @@ public abstract class WashingMachine {
 		return this.rotationSpeed;
 	}
 	
+	/**
+	 * @return This method prints out the current values for an object of the type WashingMachine
+	 */
 	public void showStatus() {
 		System.out.println("Brand: " + getBrandName() + ", programme : " + this.programme + ", temperature: " + this.temperature + "\u00b0C , speed: " + this.rotationSpeed);
 	}
